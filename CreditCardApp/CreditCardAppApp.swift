4f221a4b30cbdee2df81d034456c08ab1e813bef
iMtRo <maxmudovilyos75@gmail.com>
2022-04-06 14:30:59 +0500

    Initial Commit

---
+++ CreditCardApp/CreditCardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardAppApp.swift
+//  CreditCardApp
+//
+//  Created by user on 06/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
